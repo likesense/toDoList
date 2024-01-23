0807b81bb3303481e694171a9ce7912423978688
likesense <106629742+likesense@users.noreply.github.com>
2024-01-23 15:36:48 +0300

    Ready made form with a blank for receiving tasks.

--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -1,15 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using toDoList.Domain.ViewModels.Task;
 
 
 namespace toDoList.Controllers
 {
-    public class TaskController: Controller
+    public class TaskController : Controller
     {
 
         public IActionResult Index()
         {
             return View();
         }
-
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateTaskViewModel model)
+        {
+            return Ok();
+        }
     }
 }
